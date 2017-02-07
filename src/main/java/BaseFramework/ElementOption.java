b6b97b3b793f136fff103f8b05f2370edf8e6a14
forjone <1614743566@qq.com>
2017-02-07 17:44:50 +0800

    元素操作增加了“右击”和“双击”

--- src/main/java/BaseFramework/ElementOption.java
+++ src/main/java/BaseFramework/ElementOption.java
@@ -3,6 +3,7 @@ package BaseFramework;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 
 /**
@@ -16,8 +17,16 @@ public class ElementOption {
        /* //用来校验运行中的对象是否和对象库中的对象类型一致
         String actualType = element.getAttribute("type");
         if (actualType.equals(expectedType))
-
          */
+
+/*       操作类型：
+                  click：单击
+                  sendKeys：赋值
+                  select：下拉框选择
+                  doubleClick：双击
+                  contextClick：右击
+
+       */
             if(option.equals("click")){
 //            如果操作是“点击”的话，元素执行点击操作
                 element.click();
@@ -28,6 +37,14 @@ public class ElementOption {
                 Select select = new Select(element);
                 //        使用属性值选择下拉框
                 select.selectByValue(optionValue);
+            }else if (option.equals("doubleClick")){
+//            如果操作是“双击”，元素执行赋值操作
+                Actions actions = new Actions(driverChrome);
+                actions.doubleClick(element).perform();
+            }else if (option.equals("contextClick")){
+//            如果操作是“右击”，元素执行赋值操作
+                Actions actions = new Actions(driverChrome);
+                actions.contextClick(element).perform();
             }
             else {
                 System.out.println("此操作对象不支持，请确认操作方式是否正确，或完善代码");
